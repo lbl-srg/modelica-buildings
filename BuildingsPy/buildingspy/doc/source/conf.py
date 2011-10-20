e4904f0fc2b7b3816cf570f4c10876b30f709942
mwetter <mwetter@de040127-fb12-dd11-a555-00e0182289aa>
2011-10-20 06:17:26 +0000

    Added stylesheets

--- BuildingsPy/buildingspy/doc/source/conf.py
+++ BuildingsPy/buildingspy/doc/source/conf.py
@@ -107,10 +107,10 @@ html_theme = 'sphinxdoc'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "BuildingsPy documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+#html_short_title = 
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
