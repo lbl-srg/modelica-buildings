6c71b40f0aff46b1ca7aebeef5b3b7428feaa26b
mwetter <mwetter@lbl.gov>
2013-05-15 10:00:07 -0700

    Revised cleanHTML.py to add header

--- bin/cleanHTML.py
+++ bin/cleanHTML.py
@@ -18,6 +18,15 @@ files = [ f for f in listdir(helpDir) if f.endswith(".html") ]
 
 replacements = {'</HEAD>':
                '<link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"all\" href=\"../Resources/www/modelicaDoc.css\">\n</HEAD>', 
+               '<BODY><P>':
+               '<body>\n<!-- --- header ------ -->\n<div class="headerStyle">\n<img src="../Resources/www/lbl-logo.png" alt="LBL logo"/>\n</div>\n<div class="headerLinks">\n<ul><li><a href="http://simulationresearch.lbl.gov/modelica">Home</a> &gt; <a href="Buildings.html">Modelica</a></li></ul>\n</div>\n<!-- --- end header -- -->\n',
+
+               'file:////opt/dymola/Modelica/Library/Modelica 3.2/help':
+               '../../msl/3.2/help',
+               'file:////opt/dymola/Modelica/Library':
+               '../../msl',
+               '/home/mwetter/proj/ldrd/bie/modeling/bie/tags/releases/${VERSION}/bie/modelica/Buildings/Resources':
+               '../Resources',
                '<PRE></pre>':'', '<pre></PRE>':''}
 for fil in files:
     filNam = helpDir + os.path.sep + fil
