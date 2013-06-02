1774f9af9ee0e592b873ae7dde91bff64d8d318b
Michael Wetter <MWetter@lbl.gov>
2013-06-01 18:10:11 -0700

    Fixed bug in validateHTML.py

---
+++ bin/validateHTML.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#####################################################################
+# This script validates the html code in the info section
+# of all .mo files in the current directory and in all its
+# subdirectories.
+#
+# The script and the Validator package are still experimental
+# and its API may change.
+#
+# MWetter@lbl.gov                                          2011-06-01
+#####################################################################
+
+def validate():
+    import buildingspy.development.Validator as v
+    val = v.Validator()
+    val.validateHTMLInPackage(".")
+
+if __name__ == '__main__':
+    import sys
+    import os
+    # The path to buildingspy must be added to sys.path to work on Linux.
+    # If only added to os.environ, the Python interpreter won't find buildingspy
+    sys.path.append(os.path.join(os.path.abspath('.'), "..", "..", "BuildingsPy"))
+    validate()
+
