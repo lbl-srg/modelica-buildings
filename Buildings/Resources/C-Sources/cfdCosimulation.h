dcfb8b132728a66143a7ac6cfdfeb17c4c9d8400
zuowangda <wangda.zuo@gmail.com>
2013-08-17 17:03:08 -0400

    Added C source files for cosimulation interface

---
+++ Buildings/Resources/C-Sources/cfdCosimulation.h
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// \file   cfdCosimulation.h
+///
+/// \brief  Header filer for cosimulation with CFD
+///
+/// \author Wangda Zuo
+///         University of Miami
+///         W.Zuo@miami.edu
+///
+/// \date   8/3/2013
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+#ifndef _MODELICA_FFD_COMMON_H
+#define _MODELICA_FFD_COMMON_H
+#include "../src/FastFluidDynamics/modelica_ffd_common.h"
+#endif
+
+#pragma comment(lib, "user32.lib")
+
+CosimulationData *cosim;
+
