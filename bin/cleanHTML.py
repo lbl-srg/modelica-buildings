b1cdd2cad5c139c6d17597c9f46e3c28b847950b
Michael Wetter <mwetter@lbl.gov>
2015-04-10 10:14:51 -0700

    Updated html to avoid warning in github

--- bin/cleanHTML.py
+++ bin/cleanHTML.py
@@ -25,7 +25,7 @@ files = [ f for f in listdir(helpDir) if f.endswith(".html") ]
 replacements = {'</head>':
                '<link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" media=\"all\" href=\"../Resources/www/modelicaDoc.css\">\n</HEAD>', 
                '<body>':
-               '<body>\n<!-- --- header ------ -->\n<div class="headerStyle">\n<img src="../Resources/www/lbl-logo.png" alt="LBL logo"/>\n</div>\n<div class="headerLinks">\n<ul><li><a href="http://simulationresearch.lbl.gov/modelica">Home</a> &gt; <a href="Buildings.html">Modelica</a></li></ul>\n</div>\n<!-- --- end header -- -->\n',
+               '<body>\n<!-- begin header -->\n<div class="headerStyle">\n<img src="../Resources/www/lbl-logo.png" alt="LBL logo"/>\n</div>\n<div class="headerLinks">\n<ul><li><a href="http://simulationresearch.lbl.gov/modelica">Home</a> &gt; <a href="Buildings.html">Modelica</a></li></ul>\n</div>\n<!-- end header -->\n',
 
                'file:////opt/dymola/Modelica/Library/Modelica 3.2/help':
                '../../msl/3.2/help',
