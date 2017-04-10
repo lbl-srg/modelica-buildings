114783b74e075d3af600547108f19285ff899772
Thierry Nouidui <tsnouidui@lbl.gov>
2017-04-10 15:32:59 -0700

    Updated unit test

--- bin/runUnitTests.py
+++ bin/runUnitTests.py
@@ -37,7 +37,7 @@ def _validate_html():
     import buildingspy.development.validator as v
 
     val = v.Validator()
-    errMsg = val.validateHTMLInPackage(".")
+    errMsg = val.validateHTMLInPackage(path)
     n_msg = len(errMsg)
     for i in range(n_msg):
         if i == 0:
@@ -63,13 +63,14 @@ def _setEnvironmentVariables(var, value):
     else:
         os.environ[var] = value
 
-def _runUnitTests(batch, single_package, n_pro, show_gui):
+def _runUnitTests(batch, package, path, n_pro, show_gui):
     import buildingspy.development.regressiontest as u
 
     ut = u.Tester()
     ut.batchMode(batch)
-    if single_package is not None:
-        ut.setSinglePackage(single_package)
+    ut.setLibraryRoot(path)
+    if package is not None:
+        ut.setSinglePackage(package)
     ut.setNumberOfThreads(n_pro)
     ut.pedanticModelica(True)
     ut.showGUI(show_gui)
@@ -109,6 +110,10 @@ if __name__ == '__main__':
     unit_test_group.add_argument('-s', "--single-package",
                         metavar="Modelica.Package",
                         help="Test only the Modelica package Modelica.Package")
+    unit_test_group.add_argument("-p", "--path",
+                        default = ".",
+                        help="Path where top-level package.mo of the library is located")
+
     unit_test_group.add_argument("-n", "--number-of-processors",
                         type=int,
                         default = multiprocessing.cpu_count(),
@@ -121,8 +126,8 @@ if __name__ == '__main__':
     html_group.add_argument("--validate-html-only",
                            action="store_true")
 
-    model_group = parser.add_argument_group("arguments to check consistency of mos/mo parameters only")
-    model_group.add_argument("--validate-mo-mos-only",
+    mos_mo_group = parser.add_argument_group("arguments to check consistency of mos/mo parameters only")
+    mos_mo_group.add_argument("--validate-mo-mos-only",
                            action="store_true")
 
     # Set environment variables
@@ -149,7 +154,7 @@ if __name__ == '__main__':
 
     if args.validate_html_only:
         # Validate the html syntax only, and then exit
-        ret_val = _validate_html()
+        ret_val = _validate_html(args.path)
         exit(ret_val)
 
     if args.validate_mo_mos_only:
@@ -163,7 +168,8 @@ if __name__ == '__main__':
         single_package = None
 
     retVal = _runUnitTests(batch = args.batch,
-                           single_package = single_package,
+                           package = single_package,
+                           path = args.path,
                            n_pro = args.number_of_processors,
                            show_gui = args.show_gui)
     exit(retVal)
