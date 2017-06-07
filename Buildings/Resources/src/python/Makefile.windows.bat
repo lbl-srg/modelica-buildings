72750dde3574ace5a549e631f0a5ae5d97338814
Thierry Nouidui <tsnouidui@lbl.gov>
2017-06-07 12:13:25 -0700

    Fixed indentation[ci skip]

--- Buildings/Resources/src/python/Makefile.windows.bat
+++ Buildings/Resources/src/python/Makefile.windows.bat
@@ -36,7 +36,7 @@ IF %ERRORLEVEL% == 0 (
   ECHO Windows 32 bit compilation activated.
   SET BINDIR=..\..\Library\win32 
 )ELSE (
-	SET PYTHONInc=C:/Python27_x64/include
+    SET PYTHONInc=C:/Python27_x64/include
     SET PYTHONLibs=C:/Python27_x64/libs/python27.lib
     CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\amd64\vcvars64.bat"  >nul 2>&1
       IF ERRORLEVEL 1 (
