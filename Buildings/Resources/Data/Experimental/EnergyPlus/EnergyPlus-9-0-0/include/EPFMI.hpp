e8beb4f8ef7d3e409e6321b7b265c9af0fde3b2d
Michael Wetter <mwetter@lbl.gov>
2018-10-26 08:48:31 -0700

    Added EnergyPlus 9.0.0 [ci skip]

---
+++ Buildings/Resources/Data/Experimental/EnergyPlus/EnergyPlus-9-0-0/include/EPFMI.hpp
@@ -0,0 +1,54 @@
+#include <cstddef>
+#include <fmi2FunctionTypes.h>
+
+//typedef struct {
+//  fmi2Boolean newDiscreteStatesNeeded;
+//  fmi2Boolean terminateSimulation;
+//  fmi2Boolean nominalsOfContinuousStatesChanged;
+//  fmi2Boolean valuesOfContinuousStatesChanged;
+//  fmi2Boolean nextEventTimeDefined;
+//  fmi2Real nextEventTime; // next event if nextEventTimeDefined=fmi2True
+//} fmi2EventInfo;
+
+extern "C" {
+
+unsigned int instantiate(const char *input,
+                         const char *weather,
+                         const char *idd,
+                         const char *instanceName,
+                         const char ** parameterNames,
+                         const unsigned int parameterValueReferences[],
+                         size_t nPar,
+                         const char ** inputNames,
+                         const unsigned int inputValueReferences[],
+                         size_t nInp,
+                         const char ** outputNames,
+                         const unsigned int outputValueReferences[],
+                         size_t nOut,
+                         const char *log);
+
+unsigned int setupExperiment(double tStart,
+                             bool stopTimeDefined,
+                             const char *log);
+
+unsigned int setTime(double time,
+                     const char *log);
+
+unsigned int setVariables(const unsigned int valueReferences[],
+                          const double variablePointers[],
+                          size_t nVars1,
+                          const char *log);
+
+unsigned int getVariables(const unsigned int valueReferences[],
+                          double variablePointers[],
+                          size_t nVars2,
+                          const char *log);
+
+unsigned int getNextEventTime(fmi2EventInfo *eventInfo,
+                              const char *log);
+
+unsigned int terminate(const char *log);
+
+}
+
+
