3b28794b1b5062913af39a42f6c771b6e3175567
Kyle Benne <kyle.benne@nrel.gov>
2018-11-15 21:47:58 -0600

    Add cmake build commands

--- Buildings/Resources/C-Sources/externalObjectStructure.h
+++ Buildings/Resources/C-Sources/externalObjectStructure.h
@@ -5,6 +5,7 @@
 
 #ifndef BUILDINGS_EXTERNALOBJECTSTRUCTURE_H /* Not needed since it is only a typedef; added for safety */
 #define BUILDINGS_EXTERNALOBJECTSTRUCTURE_H
+#include <stdlib.h>
 
 typedef struct ExternalObjectStructure
 {
