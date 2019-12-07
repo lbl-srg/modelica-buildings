4dfc553e219bde966a8593b243bf04dbb8cfd4f5
Helarga <helarga@lbl.gov>
2019-12-07 11:30:04 -0800

    modified python code to reset index

--- Buildings/Resources/Data/Fluid/Chillers/IndirectAbsorptionChiller/csv_to_mos_IndirectAbsorptionChiller.py
+++ Buildings/Resources/Data/Fluid/Chillers/IndirectAbsorptionChiller/csv_to_mos_IndirectAbsorptionChiller.py
@@ -2,12 +2,15 @@
 # coding: utf-8
 import os
 import pandas as pd
+# read the whole data set
 energyplus_output = pd.read_csv('IndirectAbsorptionChiller.csv')
-# round the data
-energyplus_df_round= energyplus_output.round(6)
+#round the data command
+energyplus_output= energyplus_output.round(6)
 # copy the first line
-energyplus_1min= energyplus_df_round.loc[[0],:]
-energyplus_new = pd.concat([energyplus_df_round,energyplus_1min]).sort_index()
+energyplus_1min= energyplus_output.loc[[0],:]
+energyplus_new = pd.concat([energyplus_output,energyplus_1min]).sort_index()
+# reset index
+energyplus_new = energyplus_new.reset_index(drop=True)
 # modelica file. mos preparation
 def energyplus_to_modelica_data(file_path, timestep, data_type, file_name):
     '''
@@ -16,16 +19,16 @@ def energyplus_to_modelica_data(file_path, timestep, data_type, file_name):
     time_step: the time step of the energyplus data
     data_type: specify the data type such as float, double etc.
     file_name: the file name of the modelica data
-    '''
+    '''       
 # evaluate dimensions of the matrix
-    size = energyplus_new.shape
-# modifiy the index for modelica mos
-    energyplus_new.index = (energyplus_new.index+1)*timestep
+    size = energyplus_new.shape 
+# modifiy the index for modelica mos 
+    energyplus_new.index = (energyplus_new.index)*timestep
     energyplus_new.index.name = '# time'
-    energyplus_new.drop(['Date/Time'], axis=1, inplace=True)
+    energyplus_new.drop(['Date/Time'], axis=1, inplace=True) 
 # write to csv for modelica
     file = file_name + '.csv'
-
+    
     with open(file,'w') as f:
         line1 = '#1'
         line2 = data_type + ' ' + file_name + '(' + str(size[0]) + ',' + str(size[1]) + ')'
@@ -36,4 +39,4 @@ file_path = os.path.abspath('IndirectAbsorptionChiller.csv')
 timestep =  60
 data_type = 'float'
 file_name = 'modelica'
-energyplus_to_modelica_data(file_path, timestep, data_type, file_name)
+energyplus_to_modelica_data(file_path, timestep, data_type, file_name)  
