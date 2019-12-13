aa0a56fecd0425ce608dba8ae013d62ddddd6d64
Michael Wetter <mwetter@lbl.gov>
2019-12-12 16:34:58 -0800

    Corrected path for source file, regenerated csv file

--- Buildings/Resources/Data/Fluid/Chillers/IndirectAbsorptionChiller/csv_to_mos_IndirectAbsorptionChiller.py
+++ Buildings/Resources/Data/Fluid/Chillers/IndirectAbsorptionChiller/csv_to_mos_IndirectAbsorptionChiller.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-import os 
+import os
 import pandas as pd
+
 # read the whole data set
-energyplus_output = pd.read_csv('IndirectAbsorptionChiller.csv')
+energyplus_output = pd.read_csv(os.path.join('EnergyPlus', 'eplusout.csv'))
 #round the data command
 energyplus_output= energyplus_output.round(4)
 # copy the first line
@@ -11,24 +12,26 @@ energyplus_1min= energyplus_output.loc[[0],:]
 energyplus_output = pd.concat([energyplus_output,energyplus_1min]).sort_index()
 # reset index
 energyplus_output = energyplus_output.reset_index(drop=True)
-energyplus_output.columns= ['Date/Time','Pump[W]','QEva_flow[W]','TEvaEnt[K]','TEvaLvg[K]','mEva_flow[kg/s]','QCon_flow[W]','TConEnt[K]','TConLvg[K]','mEva_flow[kg/s]','QGen_flow[W]','QSteLoss[W]'] 
+energyplus_output.columns= ['Date/Time','Pump[W]','QEva_flow[W]','TEvaEnt[K]','TEvaLvg[K]','mEva_flow[kg/s]','QCon_flow[W]','TConEnt[K]','TConLvg[K]','mEva_flow[kg/s]','QGen_flow[W]','QSteLoss[W]']
 new = energyplus_output.loc[:,['TEvaEnt[K]','TEvaLvg[K]','TConEnt[K]','TConLvg[K]']] + float(273.15)
 energyplus_output.update(new)
+
 # modelica file. mos preparation
-def energyplus_to_modelica_data(file_path, timestep, data_type, file_name):      
+def energyplus_to_modelica_data(file_path, timestep, data_type, file_name):
 # evaluate dimensions of the matrix
-    size = energyplus_output.shape 
-# modifiy the index for modelica mos 
+    size = energyplus_output.shape
+# modifiy the index for modelica mos
     energyplus_output.index = (energyplus_output.index)*timestep
     energyplus_output.index.name = '# time'
-    energyplus_output.drop(['Date/Time'], axis=1, inplace=True) 
+    energyplus_output.drop(['Date/Time'], axis=1, inplace=True)
 # write to csv for modelica
-    file = file_name + '.csv'   
+    file = file_name + '.csv'
     with open(file,'w') as f:
         line1 = '#1'
         line2 = data_type + ' ' + file_name + '(' + str(size[0]) + ',' + str(size[1]) + ')'
         f.write('{}\n' '{}\n'.format(line1,line2))
         energyplus_output.to_csv(f, header=True)
+
 
 file_path = os.path.abspath('IndirectAbsorptionChiller.csv')
 timestep =  60
