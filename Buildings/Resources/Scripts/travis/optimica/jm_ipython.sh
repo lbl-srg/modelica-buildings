9f1f54c26ffbabd30b7fecbc224c11d6eea70d83
Michael Wetter <mwetter@lbl.gov>
2020-02-11 14:22:41 -0800

    Changed cat command

--- Buildings/Resources/Scripts/travis/optimica/jm_ipython.sh
+++ Buildings/Resources/Scripts/travis/optimica/jm_ipython.sh
@@ -121,7 +121,7 @@ echo "**************************"
 docker run ${DOCKER_FLAGS} /bin/bash -c \
   "export MODELICAPATH=${DOCKER_MODELICAPATH}:/opt/oct/ThirdParty/MSL && \
    export PYTHONPATH=${DOCKER_PYTHONPATH} && \
-   echo \"This is just a test.\" > Buildings_Controls_OBC_CDL_Continuous_Validation_LimPID_log.txt"
+   echo \"This is just a test.\" > /mnt/shared/lbl-srg/modelica-buildings/Buildings_Controls_OBC_CDL_Continuous_Validation_LimPID_log.txt"
 
 #  /opt/oct/bin/jm_ipython.sh modelica-buildings/${arg_lis}"
 retVal=$?
