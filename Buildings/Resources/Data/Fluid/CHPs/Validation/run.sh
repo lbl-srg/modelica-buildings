164f9778e45a94db2a56772da9028af486aa5b59
JayHuLBL <JianjunHu@lbl.gov>
2020-05-04 22:02:11 -0700

    updated energyplus results

--- Buildings/Resources/Data/Fluid/CHPs/Validation/run.sh
+++ Buildings/Resources/Data/Fluid/CHPs/Validation/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-EnergyPlus --readvars -d EnergyPlus -w ../../../../../../weatherdata/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw CoolingTower_VariableSpeed_Merkel.idf
+EnergyPlus --readvars -d EnergyPlus -w ../../../../weatherdata/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw MicroCogeneration_validationfile.idf
 python energyplus_to_modelica.py
 rm -rf EnergyPlus
