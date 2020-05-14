a78ac6b6119bef6f81492b925dff9edca814dc5b
Michael Wetter <MWetter@lbl.gov>
2020-05-14 15:56:43 -0700

    Updated data to E+ 9.3

--- Buildings/Resources/Data/Fluid/HeatExchangers/CoolingTowers/Validation/MerkelEnergyPlus/run.sh
+++ Buildings/Resources/Data/Fluid/HeatExchangers/CoolingTowers/Validation/MerkelEnergyPlus/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-EnergyPlus --readvars -d EnergyPlus -w ../../../../../../weatherdata/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw CoolingTower_VariableSpeed_Merkel.idf
-python energyplus_to_modelica.py
+EnergyPlus --readvars --output-directory EnergyPlus -w ../../../../../../weatherdata/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw CoolingTower_VariableSpeed_Merkel.idf
+python3 csv_to_mos.py
 rm -rf EnergyPlus
