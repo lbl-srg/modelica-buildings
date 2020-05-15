0933e1eac97964490e2e3abbe11412cb6d20d975
Michael Wetter <MWetter@lbl.gov>
2020-05-15 15:36:14 -0700

    Refactored to use new E+ scripts

---
+++ Buildings/Resources/Data/ThermalZones/EnergyPlus/Validation/RefBldgSmallOffice/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+EnergyPlus \
+  --readvars \
+  --output-directory EnergyPlus \
+  -w ../../../../../../weatherdata/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw \
+  RefBldgSmallOfficeNew2004_Chicago.idf
+python3 csv_to_mos.py
+rm -rf EnergyPlus
--- Buildings/Resources/Data/ThermalZones/EnergyPlus/Validation/TwoIdenticalZones/csv_to_mos.py
+++ Buildings/Resources/Data/ThermalZones/EnergyPlus/Validation/TwoIdenticalZones/csv_to_mos.py
@@ -1,11 +1,10 @@
-import pandas as pd
-
 #!/usr/bin/env python3
-
+import sys
+sys.path.append('../../../../../Scripts/EnergyPlus')
 import energyplus_csv_to_mos as e
 
 if __name__ == '__main__':
-  dat_fil = "TwoIdenticalZones.dat"
+  dat_fil = "CoolingTower_VariableSpeed_Merkel.dat"
   output_list =[
     "Environment:Site Outdoor Air Drybulb Temperature [C](TimeStep)",
     "Environment:Site Outdoor Air Wetbulb Temperature [C](TimeStep)",
@@ -13,5 +12,8 @@ if __name__ == '__main__':
     "THERMAL ZONE 1:Zone Air Relative Humidity [%](TimeStep)"
   ]
 
-  e.energyplus_csv_to_mos(output_list = output_list, dat_file_name=dat_fil)
-
+  e.energyplus_csv_to_mos(
+    output_list = output_list,
+    dat_file_name=dat_fil,
+    step_size=600,
+    final_time=7*24*3600)
