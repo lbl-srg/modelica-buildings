ec13a18a72527592587d10373e75f818e9113abc
Michael Wetter <mwetter@lbl.gov>
2020-05-15 17:39:15 -0700

    Added entry in validation data for t=0

--- Buildings/Resources/Data/Fluid/HeatPumps/Validation/EquationFitReversible_EnergyPlus/run.sh
+++ Buildings/Resources/Data/Fluid/HeatPumps/Validation/EquationFitReversible_EnergyPlus/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+idf_file=`ls *.idf`
 EnergyPlus \
   --readvars \
   --output-directory EnergyPlus \
   -w ../../../../../weatherdata/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw \
-  GSHPSimple-GLHE-ReverseHeatPump.idf
-python3 csv_to_mos.py GSHPSimple-GLHE-ReverseHeatPump.dat
+  ${idf_file}
+python3 csv_to_mos.py
 rm -rf EnergyPlus
