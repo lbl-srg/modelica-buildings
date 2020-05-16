8e33766cd03e6943fe37daa8b9e2ec5e871cce9f
Michael Wetter <mwetter@lbl.gov>
2020-05-15 17:50:57 -0700

    Added entry in validation data for t=0

--- Buildings/Resources/Data/ThermalZones/EnergyPlus/Validation/TwoIdenticalZones/run.sh
+++ Buildings/Resources/Data/ThermalZones/EnergyPlus/Validation/TwoIdenticalZones/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+idf_file=`ls *.idf`
 EnergyPlus \
   --readvars \
   --output-directory EnergyPlus \
   -w ../../../../../weatherdata/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw \
-  TwoIdenticalZones.idf
+  ${idf_file}
 python3 csv_to_mos.py
 rm -rf EnergyPlus
