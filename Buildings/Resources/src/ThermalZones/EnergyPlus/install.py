06b1cd7761f06d4226d58e0913b8c08a580c65b6
Michael Wetter <mwetter@lbl.gov>
2020-06-26 11:09:38 -0700

    Updated to new binary

--- Buildings/Resources/src/ThermalZones/EnergyPlus/install.py
+++ Buildings/Resources/src/ThermalZones/EnergyPlus/install.py
@@ -19,7 +19,7 @@ tar_fil="spawn.tar.gz"
 urllib.request.urlretrieve(src, tar_fil)
 
 # Find the name of the spawn executable
-fil_dic = {"bin/spawn": "", "README.md": "", "lib/libepfmi.so": "", "etc/Energy+.idd": ""}
+fil_dic = {"bin/spawn": "", "README.md": "", "lib/epfmi.so": "", "etc/Energy+.idd": ""}
 
 tar = tarfile.open(tar_fil)
 for key in fil_dic:
