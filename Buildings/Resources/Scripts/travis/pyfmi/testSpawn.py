b17875884b23d4e2ba0e19fa74885d12cfafb723
Michael Wetter <mwetter@lbl.gov>
2021-04-09 18:31:25 -0700

    Added script that reproduces non-portable FMU
    For #2443

---
+++ Buildings/Resources/Scripts/travis/pyfmi/testSpawn.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+##############################################################
+# Script that tests exporting Spawn and running it using pyfmi
+##############################################################
+
+import os
+import shutil
+
+def _create_worDir():
+    """ Create working directory
+    """
+    import os
+    import tempfile
+    import getpass
+    worDir = os.path.join(tempfile.mkdtemp(
+        prefix='tmp-modelica-buidings-spawn-test' + getpass.getuser()))
+    print(f"Created temporary directory {worDir}")
+    return worDir
+
+def simulate():
+    import subprocess
+
+    def print_output(typ, stream):
+        print(f"{typ}: {stream}")
+
+    process = subprocess.Popen(
+        ['jm_ipython.sh', 'simulate.py'],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE)
+    try:
+        stdout, stderr = process.communicate(timeout=10)
+    except TimeoutExpired:
+        process.kill()
+        stdout, stderr = process.communicate()
+        print(f"stdout: {stdout}")
+        print(f"stderr: {stderr}")
+        raise
+    if process.returncode is not 0:
+        print_output("stdout", stdout)
+        print_output("stderr", stderr)
+        raise RuntimeError("Failed to simulate fmu.")
+#    print(f"stdout: {stdout}")
+#    print(f"stderr: {stderr}")
+
+from buildingspy.simulate.Optimica import Simulator
+model = "Buildings.ThermalZones.EnergyPlus.Examples.SingleFamilyHouse.Unconditioned"
+#model = "Buildings.Controls.Continuous.Examples.LimPID"
+fmu = model.replace('.', '_') + ".fmu"
+s=Simulator(model)
+s.translate()
+
+worDir = _create_worDir()
+shutil.move(fmu, worDir)
+os.chdir(worDir)
+
+with open("simulate.py", 'w') as f:
+    f.write(f"""
+from pyfmi import load_fmu
+mod = load_fmu("{fmu}")
+mod.simulate()
+""")
+
+simulate()
+
