f0e31a8eaaf3eca0cbe596dfff995309980a4789
milica grahovac <mgrahovac@lbl.gov>
2021-09-23 16:30:01 -0700

    added further models to the variable rename lists [ci skip]

--- var_rename.sh
+++ var_rename.sh
@@ -16,8 +16,6 @@ for ff in `find . \( -name '*ControlPlusLockouts.mo' -or -name '*ControlPlusLock
      TiHea HeaLocAftCoo \
      off_within_deadband offWitDea \
      TChwRet TSlaWatRet \
-     TAirHiSet TAirHigSet \
-     TAirLoSet TAirLowSet \
           )
 
     for ((i=0; i<${#list[@]}; i+=2)); do
@@ -42,13 +40,6 @@ for ff in `find . \( -name '*AllLockouts.mo' -or -name '*AllLockouts.mos' -or -n
      TiCoo CooLocAftHea \
      TiHea HeaLocAftCoo \
      off_within_deadband offWitDea \
-     TWaLoSet TWatLowSet \
-     TChwRet TSlaWatRet \
-     TAirHiSet TAirHigSet \
-     TAirLoSet TAirLowSet \
-     TAirHiSet TAirHigSet \
-     TAirLoSet TAirLowSet \ 
-     TChwRet TSlaWaRet \
           )
 
     for ((i=0; i<${#list[@]}; i+=2)); do
@@ -62,18 +53,12 @@ for ff in `find . \( -name '*AirTemperatureLimit.mo' -or -name '*AirTemperatureL
      TAirLoSet TAirLowSet \
      htgSigAirTem heaSigAirTem \
      clgSigAirTem cooSigAirTem \
-     TAirHiSet TAirHigSet \
-     TAirLoSet TAirLowSet \
-
           )
 
     for ((i=0; i<${#list[@]}; i+=2)); do
         sed -e s/${list[i]}/${list[i+1]}/g -i $ff
     done
 done
-
-
-ChilledWaterReturnLimit
 
 for ff in `find . \( -name '*ChilledWaterReturnLimit.mo' -or -name '*ChilledWaterReturnLimit.mos' -or -name '*ChilledWaterReturnLimit.txt' -or -name '*ChilledWaterReturnLimit.svg'  \)`; do
     list=(\
@@ -106,8 +91,21 @@ done
 
 for ff in `find . \( -name '*NightFlush.mo' -or -name '*NightFlush.mos' -or -name '*NightFlush.txt' -or -name '*NightFlush.svg'  \)`; do
     list=(\
-     
+     nitFluSig uNigFlu \
+     htgSigNitFlu yNigFluHea \
+          )
 
+    for ((i=0; i<${#list[@]}; i+=2)); do
+        sed -e s/${list[i]}/${list[i+1]}/g -i $ff
+    done
+done
+
+for ff in `find . \( -name '*AirTemperatureLockout.mo' -or -name '*AirTemperatureLockout.mos' -or -name '*AirTemperatureLockout.txt' -or -name '*AirTemperatureLockout.svg'  \)`; do
+    list=(\
+     TAirHiLim TAirHigLim \
+     TAirLoLim TAirLowLim \
+     THiRoo THigRoo \
+     TLoRoo TLowRoo \
           )
 
     for ((i=0; i<${#list[@]}; i+=2)); do
@@ -116,4 +114,70 @@ for ff in `find . \( -name '*NightFlush.mo' -or -name '*NightFlush.mos' -or -nam
 done
 
 
+for ff in `find . \( -name '*ChilledWaterReturnLockout.mo' -or -name '*ChilledWaterReturnLockout.mos' -or -name '*ChilledWaterReturnLockout.txt' -or -name '*ChilledWaterReturnLockout.svg'  \)`; do
+    list=(\
+     TWaLoSet TWaLowSet \
+     TiCHW CooLocDurWatTem \
+          )
 
+    for ((i=0; i<${#list[@]}; i+=2)); do
+        sed -e s/${list[i]}/${list[i+1]}/g -i $ff
+    done
+done
+
+
+for ff in `find . \( -name '*HysteresisLockout.mo' -or -name '*HysteresisLockout.mos' -or -name '*HysteresisLockout.txt' -or -name '*HysteresisLockout.svg'  \)`; do
+    list=(\
+     TiCoo CooLocAftHea \
+     TiHea HeaLocAftCoo \
+          )
+
+    for ((i=0; i<${#list[@]}; i+=2)); do
+        sed -e s/${list[i]}/${list[i+1]}/g -i $ff
+    done
+done
+
+
+for ff in `find . \( -name '*NightFlushLockout.mo' -or -name '*NightFlushLockout.mos' -or -name '*NightFlushLockout.txt' -or -name '*NightFlushLockout.svg'  \)`; do
+    list=(\
+     nitFluLoc nigFluLoc \
+     TiHea HeaLocAftCoo \
+          )
+
+    for ((i=0; i<${#list[@]}; i+=2)); do
+        sed -e s/${list[i]}/${list[i+1]}/g -i $ff
+    done
+done
+
+
+for ff in `find . \( -name '*AllLockout.mo' -or -name '*AllLockout.mos' -or -name '*AllLockout.txt' -or -name '*AllLockout.svg'  \)`; do
+    list=(\
+     TAirHiLim TAirHigLim \
+     TAirLoLim TAirLowLim \
+     TAirHiSet TAirHigSet \
+     TAirLoSet TAirLowSet \
+     TimeCHW LocDurCHW \
+     TimHea LocDurHea \
+     TimCoo LocDurCoo \
+     TWaLoSet TWatLowSet \
+     TiCHW CooLocDurWatTem \
+     TiCoo CooLocAftHea \
+     TiHea HeaLocAftCoo \
+          )
+
+    for ((i=0; i<${#list[@]}; i+=2)); do
+        sed -e s/${list[i]}/${list[i+1]}/g -i $ff
+    done
+done
+
+
+for ff in `find . \( -name '*DeadbandControl.mo' -or -name '*DeadbandControl.mos' -or -name '*DeadbandControl.txt' -or -name '*DeadbandControl.svg'  \)`; do
+    list=(\
+     nitFluLoc nigFluLoc \
+     TiHea HeaLocAftCoo \
+          )
+
+    for ((i=0; i<${#list[@]}; i+=2)); do
+        sed -e s/${list[i]}/${list[i+1]}/g -i $ff
+    done
+done
