0c248853495e7a021609787803a66691ab652f1f
Michael Wetter <mwetter@lbl.gov>
2021-11-16 08:44:01 -0800

    Correct segmentation fault if logging is set to debug and variable in E+ does not specify a unit.

--- Buildings/Resources/src/ThermalZones/EnergyPlus/C-Sources/SpawnUtil.c
+++ Buildings/Resources/src/ThermalZones/EnergyPlus/C-Sources/SpawnUtil.c
@@ -207,9 +207,15 @@ void getVariables(FMUBuilding* bui, const char* modelicaInstanceName, spawnReals
   /* If debug mode, write exchanged values to log file */
   if (bui->logLevel >= TIMESTEP){
     for(i = 0; i < ptrReals->n; i++){
-      bui->SpawnFormatMessage("%.3f %s: Received from EnergyPlus, %s = %.6g [%s].\n",
-        bui->time, modelicaInstanceName, ptrReals->fmiNames[i], ptrReals->valsEP[i],
-        fmi2_import_get_unit_name(ptrReals->units[i]));
+      if (ptrReals->units[i]){ /* Units are defined */
+        bui->SpawnFormatMessage("%.3f %s: Received from EnergyPlus, %s = %.6g [%s].\n",
+          bui->time, modelicaInstanceName, ptrReals->fmiNames[i], ptrReals->valsEP[i],
+          fmi2_import_get_unit_name(ptrReals->units[i]));
+      }
+      else{
+        bui->SpawnFormatMessage("%.3f %s: Received from EnergyPlus, %s = %.6g (no units declared).\n",
+          bui->time, modelicaInstanceName, ptrReals->fmiNames[i], ptrReals->valsEP[i]);
+      }
     }
   }
 
