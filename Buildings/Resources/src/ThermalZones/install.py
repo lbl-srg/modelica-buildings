5d847001b4a22b209c954ff689919b5f79437683
Michael Wetter <mwetter@lbl.gov>
2022-05-20 06:01:10 -0700

    Added dummy script

---
+++ Buildings/Resources/src/ThermalZones/install.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#######################################################
+# Dummy file that will be replaced with the Spawn
+# installation script through
+# https://github.com/lbl-srg/modelica-buildings/pull/3020
+#######################################################
+import subprocess
+import argparse
+import os
+
+if __name__ == '__main__':
+    print("Dummy script that will be replaced with the Spawn installation script.")
