b90fe0c17ab19f59aedd1a25ff36e9ff4bfcfae0
Michael Wetter <mwetter@lbl.gov>
2022-05-23 04:42:05 -0700

    Issue3018 spawn installation (#3020)
    * Removed Resources/bin/spawn-0.3.0-59ed8c72e4
    
    For #3018
    
    * Corrected hash in file name
    
    * Updated install script to support multiple versions
    
    * Changed spawn install folder to Spawn-light-version-commit
    
    * Added EnergyPlus binary installation
    
    * Updated install section
    
    * Updated doc, added Windows binaries, added install.py

--- Buildings/Resources/src/ThermalZones/install.py
+++ Buildings/Resources/src/ThermalZones/install.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 #######################################################
-# Dummy file that will be replaced with the Spawn
-# installation script through
-# https://github.com/lbl-srg/modelica-buildings/pull/3020
+# Script that installs the binaries that are
+# used by Spawn.
 #######################################################
 import subprocess
 import argparse
 import os
 
 if __name__ == '__main__':
-    print("Dummy script that will be replaced with the Spawn installation script.")
+
+  # Configure the argument parser
+  parser = argparse.ArgumentParser(
+      description='Install and updates files used by Spawn.',
+      allow_abbrev=False)
+
+  parser.add_argument("--binaries-for-os-only",
+                      action="store_true",
+                      help="Only install binaries needed for the current operating system.")
+
+  # Parse the arguments
+  args = parser.parse_args()
+
+  proc= os.path.join(os.path.dirname(os.path.realpath(__file__)),  "EnergyPlus_9_6_0", "install.py")
+
+  if args.binaries_for_os_only:
+    proc = f"{proc} --binaries-for-os-only"
+
+  subprocess.run(proc, shell=True)
