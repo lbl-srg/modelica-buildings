4402c76601e59b3a76fdd1c675bde64ec975a234
Devaprasad <karthikeya.devaprasad@pnnl.gov>
2022-06-14 11:00:05 -0700

    Added new idf files and updated bash and python scripts to run them and process their results

--- Buildings/Resources/Data/Fluid/ZoneEquipment/csv_to_mos.py
+++ Buildings/Resources/Data/Fluid/ZoneEquipment/csv_to_mos.py
@@ -3,26 +3,36 @@ import sys
 sys.path.append('../../../Scripts/EnergyPlus')
 import energyplus_csv_to_mos as e
 
-if __name__ == '__main__':
-  dat_fil = "FanCoil.dat"
-  output_list =[
-   "ZONE1FANCOIL:Fan Coil Heating Rate [W](TimeStep)",
-   "ZONE1FANCOIL:Fan Coil Total Cooling Rate [W](TimeStep)",
-   "ZONE1FANCOIL:Fan Coil Fan Electricity Rate [W](TimeStep)",
-   "ZONE1FANCOILAIROUTLETNODE:System Node Temperature [C](TimeStep)",
-   "ZONE1FANCOILAIRINLETNODE:System Node Temperature [C](TimeStep)",
-   "ZONE1FANCOILAIRINLETNODE:System Node Mass Flow Rate [kg/s](TimeStep)",
-   "ZONE1FANCOILCHWINLETNODE:System Node Temperature [C](TimeStep)",
-   "ZONE1FANCOILCHWINLETNODE:System Node Mass Flow Rate [kg/s](TimeStep)",
-   "ZONE1FANCOILHWINLETNODE:System Node Temperature [C](TimeStep)",
-   "ZONE1FANCOILHWINLETNODE:System Node Mass Flow Rate [kg/s](TimeStep)",
-   "ZONE1FANCOILAIROUTLETNODE:System Node Mass Flow Rate [kg/s](TimeStep)",
-   "WEST ZONE:Zone Thermostat Heating Setpoint Temperature [C](TimeStep)",
-   "WEST ZONE:Zone Thermostat Cooling Setpoint Temperature [C](TimeStep)"
-  ]
+def check():
+    args = sys.argv[1:]
+    dat_fil = args[0].replace(".idf", ".dat")
+    print(dat_fil)
 
-  e.energyplus_csv_to_mos(
+def main():
+    args = sys.argv[1:]
+    dat_fil = args[0]
+    output_list =[
+    "ZONE1FANCOIL:Fan Coil Heating Rate [W](TimeStep)",
+    "ZONE1FANCOIL:Fan Coil Total Cooling Rate [W](TimeStep)",
+    "ZONE1FANCOIL:Fan Coil Fan Electricity Rate [W](TimeStep)",
+    "ZONE1FANCOILAIROUTLETNODE:System Node Temperature [C](TimeStep)",
+    "ZONE1FANCOILAIRINLETNODE:System Node Temperature [C](TimeStep)",
+    "ZONE1FANCOILAIRINLETNODE:System Node Mass Flow Rate [kg/s](TimeStep)",
+    "ZONE1FANCOILCHWINLETNODE:System Node Temperature [C](TimeStep)",
+    "ZONE1FANCOILCHWINLETNODE:System Node Mass Flow Rate [kg/s](TimeStep)",
+    "ZONE1FANCOILHWINLETNODE:System Node Temperature [C](TimeStep)",
+    "ZONE1FANCOILHWINLETNODE:System Node Mass Flow Rate [kg/s](TimeStep)",
+    "ZONE1FANCOILAIROUTLETNODE:System Node Mass Flow Rate [kg/s](TimeStep)",
+    "WEST ZONE:Zone Thermostat Heating Setpoint Temperature [C](TimeStep)",
+    "WEST ZONE:Zone Thermostat Cooling Setpoint Temperature [C](TimeStep)",
+    "WEST ZONE:Zone Air Temperature [C](TimeStep)"
+    ]
+
+    e.energyplus_csv_to_mos(
     output_list = output_list,
     dat_file_name=dat_fil,
     step_size=60,
     final_time=31536000)
+
+if __name__ == '__main__':
+    main()
--- Buildings/Resources/Data/Fluid/ZoneEquipment/run.sh
+++ Buildings/Resources/Data/Fluid/ZoneEquipment/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
-idf_file=`ls *.idf`
+idf_files="."/*.idf
+for entry in $idf_files
+do
+echo "$entry"
 energyplus \
   --readvars \
   --output-directory EnergyPlus \
   -w ../../../weatherdata/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw \
-  ${idf_file}
-python3 csv_to_mos.py
+  "$entry"
+python3 csv_to_mos.py "$entry"
 rm -rf EnergyPlus
+done
