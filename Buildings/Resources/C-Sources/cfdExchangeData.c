2363366dc7305a9b4173d27860364c1495eabb79
caryfaulkner <cary.faulkner@colorado.edu>
2022-11-21 13:35:37 -0700

    Fixed formatting

--- Buildings/Resources/C-Sources/cfdExchangeData.c
+++ Buildings/Resources/C-Sources/cfdExchangeData.c
@@ -95,11 +95,13 @@ int cfdExchangeData(double t0, double dt, double *u, size_t nU, size_t nY,
   --------------------------------------------------------------------------*/
   /* If previous data hasn't been read, wait*/
   while(cosim->modelica->flag==1) {
-    if(cosim->para->ffdError==1)
+    if(cosim->para->ffdError==1) {
       ModelicaError(cosim->ffd->msg);
 	  return -1;
-    else
+    }
+    else {
       Sleep(10);
+    }
   }
 
   cosim->modelica->t = t0;
@@ -160,11 +162,13 @@ int cfdExchangeData(double t0, double dt, double *u, size_t nU, size_t nY,
   ****************************************************************************/
   /* If the data is not ready or not updated, check again*/
   while(cosim->ffd->flag!=1) {
-    if(cosim->para->ffdError==1)
+    if(cosim->para->ffdError==1) {
       ModelicaError(cosim->ffd->msg);
       return -1;
-    else
+    }
+    else {
       Sleep(10);
+    }
   }
 
   /* Get the temperature/heat flux for solid surface*/
