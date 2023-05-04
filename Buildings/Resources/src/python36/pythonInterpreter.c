c8fd257a6c5184e7dee33dd525794c601f923292
Michael Wetter <mwetter@lbl.gov>
2023-05-04 09:55:20 -0700

    Reverted Python 3.6 files
    These are obsolete and I suggest to keep this old code (and maybe remove it for 10.0.0)

--- Buildings/Resources/src/python36/pythonInterpreter.c
+++ Buildings/Resources/src/python36/pythonInterpreter.c
@@ -41,7 +41,7 @@ static void saveAppendModelica(
 	/* reallocate memory if needed */
 	if (*bufLen < nNewCha + nBufCha + 1) {
 		*bufLen = *bufLen + nNewCha + minInc + 1;
-		*buffer = (char *) realloc(*buffer, *bufLen * sizeof(char));
+		*buffer = realloc(*buffer, *bufLen);
 		if (*buffer == NULL) {
 			ModelicaFormatError("Realloc failed in saveAppendModelica %s.", moduleName);
 		}
@@ -55,7 +55,7 @@ static void saveAppendModelica(
 /* Create the structure and initialize its pointer to NULL. */
 void* initPythonMemory()
 {
-	pythonPtr* ptr = (pythonPtr*) malloc(sizeof(pythonPtr));
+	pythonPtr* ptr = malloc(sizeof(pythonPtr));
 	/* Set ptr to null as pythonExchangeValuesNoModelica is checking for this */
 	ptr->ptr = NULL;
 	ptr->isInitialized = 0;
@@ -95,7 +95,7 @@ void pythonExchangeValuesNoModelica(const char * moduleName,
 
 	if (ptrMemory->pythonPath == NULL) {
 		/* Construct the python path */
-		ptrMemory->pythonPath = (char*) malloc(sizeof(char) * (lenPath + 1));
+		ptrMemory->pythonPath = malloc(sizeof(char) * (lenPath + 1));
 		if (ptrMemory->pythonPath == NULL) {
 			ModelicaFormatError("Failed to allocate memory for PYTHONPATH in pythonExchangeValuesNoModelica for %s.", moduleName);
 		}
