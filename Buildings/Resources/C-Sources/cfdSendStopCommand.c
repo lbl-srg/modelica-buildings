707423129c842c5aed6bbd0d0bc0a6b5c44bcf81
Michael Wetter <mwetter@lbl.gov>
2024-09-12 12:45:57 -0700

    Removed whitespace

--- Buildings/Resources/C-Sources/cfdSendStopCommand.c
+++ Buildings/Resources/C-Sources/cfdSendStopCommand.c
@@ -149,7 +149,7 @@ void cfdSendStopCommand(void *thread) {
   }
   if (cosim != NULL){
     free(cosim);
-    /* If it is not explicitly set to NULL - then the check in cfdcosim() 
+    /* If it is not explicitly set to NULL - then the check in cfdcosim()
        will fail if a second simulation is performed. */
     cosim = NULL;
   }
