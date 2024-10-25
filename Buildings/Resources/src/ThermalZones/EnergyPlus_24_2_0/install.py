05958b943dadef2b16b9b35ba911d732d5f09881
Michael Wetter <mwetter@lbl.gov>
2024-10-25 11:43:11 -0700

    Updated Spawn to 0.6.0

--- Buildings/Resources/src/ThermalZones/EnergyPlus_24_2_0/install.py
+++ Buildings/Resources/src/ThermalZones/EnergyPlus_24_2_0/install.py
@@ -23,7 +23,7 @@ import shutil
 # build_type is either custom or builds
 spawn_dists = [
     {"version": "0.6.0",
-     "commit": "69002307aa",
+     "commit": "3794215efe",
      "build_type": "custom"}
 ]
 ###########################################################################
