9569ff40430ff53506556c04bb9bf944e6e03fcf
Michael Wetter <mwetter@lbl.gov>
2025-03-13 06:09:24 -0700

    Updated path to relative directory

--- Buildings/Resources/Data/Obsolete/ThermalZones/EnergyPlus_9_6_0/Examples/RefBldgSmallOffice/csv_to_mos.py
+++ Buildings/Resources/Data/Obsolete/ThermalZones/EnergyPlus_9_6_0/Examples/RefBldgSmallOffice/csv_to_mos.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sys
-sys.path.append('../../../../../Scripts/EnergyPlus')
+sys.path.append('../../../../../../Scripts/EnergyPlus')
 import energyplus_csv_to_mos as e
 
 if __name__ == '__main__':
