1f73842b3f93100aea1973164aba040ce7b0515d
Michael Wetter <mwetter@lbl.gov>
2026-06-17 18:27:00 -0700

    Added test for wrong annotations

--- bin/verifyFiles.py
+++ bin/verifyFiles.py
@@ -61,6 +61,7 @@ INVALID_REGEXP_IN_MO=[r"StopTime\s*=\s*\d\s*[*]\s*\d+",
                       r"file\s*=\s*\"Resources", # This should be file="modelica://Buildings/Resources
                       r"parameter.*Boolean.*homotopyInitialization",
                       r"(Text\s*\([^\)]*)lineColor",
+                      r'(Text\(\s*)textString\s*=\s*"[^"]*"\s*,\s*',
                       r"(Line\s*\([^\)]*)lineThickness"]
 # List of strings that are required in .mo files, except in Examples
 REQUIRED_IN_MO=["documentation"]
