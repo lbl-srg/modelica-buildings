d0c53904e1fbd85d9425c675ced54f7d625dee11
Michael Wetter <MWetter@lbl.gov>
2026-07-09 19:36:38 -0700

    Added header files for #4658

---
+++ Buildings/Resources/C-Sources/Modelica_EnergyPlus_24_2_0_getParameters.h
@@ -0,0 +1,8 @@
+#ifndef Modelica_EnergyPlus_24_2_0_getParameters_h
+#define Modelica_EnergyPlus_24_2_0_getParameters_h
+
+#include "EnergyPlus_24_2_0_definitions.h"
+
+extern void getParameters_Spawn_EnergyPlus_24_2_0(void* object, double *parOut);
+
+#endif
